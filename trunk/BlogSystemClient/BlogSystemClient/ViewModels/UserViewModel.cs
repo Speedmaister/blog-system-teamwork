f563db820647b2ab467a9800009172c97205243d
gardax95@gmail.com <gardax95@gmail.com@1177a7c7-3823-f46b-9e1b-1c72dd53f900>
2013-09-14 12:07:02 +0000

    add some views

---
+++ trunk/BlogSystemClient/BlogSystemClient/ViewModels/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BlogSystemClient.Commands;
+using BlogSystemClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BlogSystemClient.ViewModels
+{
+    public class UserViewModel:UserModel
+    {
+        private bool loginCommand;
+        public string SessionKey { get; set; }
+
+        public ICommand Login
+        {
+            get 
+            {
+                if(this.loginCommand==null)
+                {
+                    this.loginCommand = new RelayCommand();
+                }
+            }
+        }
+    }
+}
